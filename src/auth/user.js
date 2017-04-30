22924ba7f004d135ad0d62c69c7372f19a15e407
Jaguar Sparq <kifhan@gmail.com>
2017-04-30 17:28:19 +0900

    fix for alpha

--- src/auth/user.js
+++ src/auth/user.js
@@ -8,10 +8,11 @@ function User () {
     self.user_picture = './static/img/User-Profile.png'
     self.guest_user_el = '<div style="display:inline-block"><img width="32px" src="'+ self.user_picture +'"></div>'
     window.document.getElementById('status').innerHTML = self.guest_user_el;
-    window.setUserData = self.setData
+    window.setUserData = self.setData.bind(self)
 }
 
 User.prototype.setData = function (data) {
+    var self = this
     self.user_id = data.user_id
     self.user_name = data.user_name
     self.user_picture = data.user_picture
